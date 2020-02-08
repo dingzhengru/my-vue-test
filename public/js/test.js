76ef7c90de9cc80f304947b8bb78c71140b30065
dingzhengru <qws7869vdx@gmail.com>
2020-02-08 21:45:28 +0800

    new filter

--- public/js/test.js
+++ public/js/test.js
@@ -437,3 +437,50 @@ var app19 = new Vue({
         },
     }
 })
+
+var app20 = new Vue({
+    el: '#app20',
+    data: {
+        nums: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    },
+    filters: {
+        evens: function(value) {
+            // value = [0,1,2,3,4,5,6,7,8,9,10,11] (nums)
+            let newArray = []
+            for(let i in value) {
+                if(value[i] % 2 == 0) {
+                    newArray.push(value[i])
+                }
+            }
+            return newArray
+        },
+        odds: function(value) {
+            // value = [0,1,2,3,4,5,6,7,8,9,10] (nums)
+            let newArray = []
+            for(let i in value) {
+                if(value[i] % 2 == 1) {
+                    newArray.push(value[i])
+                }
+            }
+            return newArray
+        },
+        factor3: function(value) {
+            let newArray = []
+            for(let i in value) {
+                if(value[i] % 3 == 0) {
+                    newArray.push(value[i])
+                }
+            }
+            return newArray
+        },
+        factor5: function(value) {
+            let newArray = []
+            for(let i in value) {
+                if(value[i] % 5 == 0) {
+                    newArray.push(value[i])
+                }
+            }
+            return newArray
+        }
+    }
+})
